51070e2a4b412c86cbe951d46895b968bea3fc61
bagdenia <e.ivanova@potok.digital>
2017-12-08 09:18:46 +0300

    Whole push commit

---
+++ data.js
@@ -0,0 +1,43 @@
+module.exports = {
+  items: [
+  { 
+    id: 1,
+    image: {
+      src: '/dist/images/1.jpg',
+      alt: 'hi kitty'
+    },
+    meta: {
+      name: 'Fedya', 
+      likes: 4,
+      dateCreated: '2017-10-02'
+    },
+    text: 'Hello all'
+  },
+  { 
+    id: 2,   
+    image: {
+      src: '/dist/images/2.jpg',
+      alt: 'hi kitty'
+    },
+    meta: {
+      name: 'Vasya', 
+      likes: 7,
+      dateCreated: '2017-09-07'
+    },
+    text: 'Bye all'
+  },
+  {
+    id: 3, 
+    image: {
+      src: '/dist/images/3.jpg',
+      alt: 'hi kitty'
+    },
+    meta: {
+      dateCreated: '2017-09-07'
+    },
+    text: 'Nice day for'
+  }
+]
+};
+
+
---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var application = express();
+
+var cors = require('cors');
+
+var items = require('./data').items;
+
+application.use(cors());
+
+application.get('/', function(req, res) {
+  res.json(items);
+});
+  
+application.get('/posts/:id', function(req, res) {
+ res.json(items[req.params.id-1]);
+});
+ 
+application.listen(5001, function() {
+  console.log('Server on 5001')
+});
