0614d480833d5b4c10c3f0c7d0f1a4730707347f
bagdenia <e.ivanova@potok.digital>
2018-01-08 14:55:03 +0300

    Server side for lesson 7 post likes

--- index.js
+++ index.js
@@ -7,13 +7,21 @@ var items = require('./data').items;
 
 application.use(cors());
 
-application.get('/', function(req, res) {
-  res.json(items);
+application.get('/', function (req, res) {
+  var text = req.query.text;
+  var matched = items.filter((entry) => entry.text.match(new RegExp(text, 'i')));
+  res.json(matched);
 });
   
-application.get('/posts/:id', function(req, res) {
+application.get('/posts/:id', function (req, res) {
  res.json(items[req.params.id-1]);
 });
+
+application.post('/', function (req, res) {
+    var item = items[req.query.id - 1];
+    item.meta.likes += 1;
+    res.send(item);
+  });
  
 application.listen(5001, function() {
   console.log('Server on 5001')
